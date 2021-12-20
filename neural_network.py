f60fced863c392b52e9ebbb40f5536bc0ab43b61
Mateusz Bronislaw Wasilewski <01158977@pw.edu.pl>
2021-12-20 10:05:39 +0100

    Initial implementation for backpropagation in output layer

--- neural_network.py
+++ neural_network.py
@@ -1,5 +1,6 @@
-import pandas as pd
+# import pandas as pd
 import numpy as np
+
 
 class NeuralNetwork:
     def __init__(self, input_neurons=11, hidden_neurons=11, output_neurons=11):
@@ -14,28 +15,49 @@ class NeuralNetwork:
             [0.5] * self.output_neurons_num
             for _ in range(self.hidden_neurons_num)
         ]
+        self.learning_rate = 0.01
 
-    def activation_func(self, x):
+    def sigmoid(self, x, deriv=False):
+        if deriv is True:
+            return x * (1 - x)
         return 1 / (1 + np.exp(-x))
 
-    def back_propagation(self):
-        pass
-
     def calculations_in_hidden_layer(self, input):
-        self.hidden_neurons_val = [
-            self.activation_func(np.dot(input, neuron))
-            for neuron in self.input_weights
-        ]
+        # self.hidden_neurons_val = [
+        #     self.sigmoid(np.dot(input, neuron))
+        #     for neuron in self.input_weights
+        # ]
+        self.hidden_neurons_val = self.sigmoid(
+            np.dot(input, self.input_weights)
+        )
 
     def calculations_in_output_neurons(self):
-        # self.output_neurons_val = [
-        #     np.dot(hidden, output)
-        #     for hidden, output in zip(self.hidden_neurons_val, self.output_weights)
-        # ]
-        self.output_neurons_val = [
-            np.dot(self.hidden_neurons_val, output)
-            for output in self.output_weights
+        self.output_neurons_val = np.dot(
+            self.hidden_neurons_val, self.output_weights
+        )
+
+    def get_mean_square_errors(self, expected_class):
+        self.expected_output = [0] * self.output_neurons_num
+        self.expected_output[int(expected_class)] = 1
+        # self.expected_output = np.asarray(self.expected_output)
+        self.errors = [
+            # 0.5 * (output - error) ** 2
+            output - error
+            for output, error in zip(
+                self.output_neurons_val, self.expected_output
+            )
         ]
+        self.errors = np.asarray(self.errors)
+
+    def back_propagation(self):
+        delta = [
+            error * self.sigmoid(output, True)
+            for error, output in zip(self.errors, self.output_neurons_val)
+        ]
+        for i in range(self.output_neurons_num):
+            self.output_weights[i] += self.learning_rate * np.dot(
+                self.hidden_neurons_val[i], delta[i]
+            )
 
     def train(self, training_data, epochs=1):
         for _ in range(epochs):
@@ -45,6 +67,8 @@ class NeuralNetwork:
                 values = row[:-1]
                 self.calculations_in_hidden_layer(values)
                 self.calculations_in_output_neurons()
+                self.get_mean_square_errors(class_value)
+                self.back_propagation()
 
     def predict(self):
         pass
--- run.py
+++ run.py
@@ -1,16 +1,18 @@
-import pandas as pd
+# import pandas as pd
 import numpy as np
 from neural_network import NeuralNetwork
 from get_data import get_data
 
+
 def run():
     data = get_data()
-    data = np.asarray(data)
-    print(data)
+    data = np.asarray(data[:1])
+    # print(data)
     # data = pd.read_csv('winequality-red.csv')
     nn = NeuralNetwork()
     nn.train(data)
-    pass
+    print(nn.input_weights)
+    print(nn.output_weights)
 
 
 if __name__ == "__main__":
